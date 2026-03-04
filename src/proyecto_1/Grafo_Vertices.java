597165cf53d127ca7dda0ad718213811d9aaa0e2
coco <VeraCorina>
2026-03-03 23:53:22 -0400

    Grafo_Vertices inicio

---
+++ src/proyecto_1/Grafo_Vertices.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_1;
+
+/**
+ *
+ * @author coco
+ */
+public class Grafo_Vertices {
+
+    private Nodo_Vertice pFirst;
+    private Nodo_Vertice pLast;
+    private int pSize;
+
+    public Grafo_Vertices() {
+        this.pFirst = null;
+        this.pLast = null;
+        this.pSize = 0;
+    }
+
+    /**
+     * @return the pFirst
+     */
+    public Nodo_Vertice getpFirst() {
+        return pFirst;
+    }
+
+    /**
+     * @param pFirst the pFirst to set
+     */
+    public void setpFirst(Nodo_Vertice pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    /**
+     * @return the pLast
+     */
+    public Nodo_Vertice getpLast() {
+        return pLast;
+    }
+
+    /**
+     * @param pLast the pLast to set
+     */
+    public void setpLast(Nodo_Vertice pLast) {
+        this.pLast = pLast;
+    }
+
+    /**
+     * @return the pSize
+     */
+    public int getpSize() {
+        return pSize;
+    }
+
+    /**
+     * @param pSize the pSize to set
+     */
+    public void setpSize(int pSize) {
+        this.pSize = pSize;
+    }
+
+
+    
+}
