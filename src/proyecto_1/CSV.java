b081a6fd975f60a43c974959cbe55de3b4bd931b
Natalio <natodjandji>
2026-03-06 17:36:34 -0400

    CAMBIOS CSV VENTANA

--- src/proyecto_1/CSV.java
+++ src/proyecto_1/CSV.java
@@ -11,12 +11,14 @@ package proyecto_1;
 public class CSV extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(CSV.class.getName());
-
+    static Grafo_Vertices g ;
     /**
      * Creates new form CSV
      */
-    public CSV() {
+    public CSV(Grafo_Vertices g) {
         initComponents();
+        this.g = g;
+        this.setVisible(true);
     }
 
     /**
@@ -31,10 +33,8 @@ public class CSV extends javax.swing.JFrame {
         pan = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -45,46 +45,52 @@ public class CSV extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Verdana", 3, 48)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel1.setText("MENU INICIAL");
-        pan.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 60, -1, -1));
+        jLabel1.setText("OPCIONES CSV");
+        pan.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 40, -1, -1));
 
         jButton1.setBackground(new java.awt.Color(153, 153, 153));
         jButton1.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
         jButton1.setForeground(new java.awt.Color(255, 255, 255));
         jButton1.setText("Regresar");
         jButton1.addActionListener(this::jButton1ActionPerformed);
-        pan.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(802, 20, 160, 40));
-
-        jTextField1.setBackground(new java.awt.Color(153, 153, 153));
-        jTextField1.setFont(new java.awt.Font("Verdana", 2, 24)); // NOI18N
-        jTextField1.setForeground(new java.awt.Color(255, 255, 255));
-        pan.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 240, 200, -1));
-
-        jTextArea1.setBackground(new java.awt.Color(153, 153, 153));
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Verdana", 2, 18)); // NOI18N
-        jTextArea1.setForeground(new java.awt.Color(255, 255, 255));
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
-
-        pan.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 320, -1, 230));
+        pan.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 50, 160, 40));
 
         jButton2.setBackground(new java.awt.Color(153, 153, 153));
         jButton2.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
         jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("jButton1");
-        pan.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 160, -1, -1));
+        jButton2.setText("Guardar");
+        jButton2.addActionListener(this::jButton2ActionPerformed);
+        pan.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 240, -1, -1));
 
-        getContentPane().add(pan, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1010, 590));
+        jButton3.setBackground(new java.awt.Color(153, 153, 153));
+        jButton3.setFont(new java.awt.Font("Verdana", 1, 24)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(255, 255, 255));
+        jButton3.setText("Cargar");
+        jButton3.addActionListener(this::jButton3ActionPerformed);
+        pan.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 160, 140, -1));
+
+        getContentPane().add(pan, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 810, 390));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        Interfaz inter = new Interfaz();
+        Interfaz inter = new Interfaz(g);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        ManejadorArchivos m = new ManejadorArchivos();
+        m.ejecutarSelectorCarga(g);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        ManejadorArchivos m = new ManejadorArchivos();
+        m.ejecutarSelectorGuardado(g);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -108,16 +114,14 @@ public class CSV extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> new CSV().setVisible(true));
+        java.awt.EventQueue.invokeLater(() -> new CSV(g).setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JPanel pan;
     // End of variables declaration//GEN-END:variables
 }
