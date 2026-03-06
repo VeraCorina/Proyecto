b2eebe1f3c4da76e74d53a092368a65cbf120c17
coco <VeraCorina>
2026-03-06 17:55:19 -0400

    corregida cola y javadoc

--- src/proyecto_1/Cola.java
+++ src/proyecto_1/Cola.java
@@ -8,15 +8,27 @@ package proyecto_1;
  *
  * @author coco
  */
+/**
+ * esta clase es para armar una cola normalita de toda la vida
+ * sirve para ir guardando los nodos y sacarlos en orden
+ * * @author coco
+ */
 public class Cola {
     NodoCola pFirst;
     NodoCola pLast;
     
+    /**
+     * el constructor que pone todo en null para empezar de cero
+     */
     public Cola(){
         pFirst = null;
         pLast = null;
     }
     
+    /**
+     * mete un dato nuevo al final de la fila
+     * @param dato el vertice que queremos encolar
+     */
     public void encolar(Nodo_Vertice dato){
         NodoCola nuevo = new NodoCola(dato);
         if(this.pFirst != null){
@@ -28,6 +40,10 @@ public class Cola {
         }
     }
     
+    /**
+     * saca al primero de la cola porque ya le toca
+     * @return el nodo que estaba de primero o null si no hay nadie
+     */
     public NodoCola desencolar(){
         if(this.pFirst != null){
             NodoCola temp = this.pFirst;
